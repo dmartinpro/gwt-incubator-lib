e0f9191985df82a368208fe2c42a7e53c5f881c0
dmartin.pro <dmartin.pro@14f895b6-ede3-11dd-b787-91885b911a96>
2009-02-09 20:52:17 +0000

    refactoring / bug fixes / enhancements

---
+++ trunk/GWT-Incubator-TimePicker/src/main/java/com/gwtincubator/client/widget/timepicker/TimeMode.java
@@ -0,0 +1,27 @@
+package com.gwtincubator.client.widget.timepicker;
+
+/* package */ enum TimeMode {
+
+	AM_MODE("am"),
+	PM_MODE("pm");
+
+	private String mode;
+
+	TimeMode(final String mode) {
+		this.mode = mode;
+	}
+
+	public String getMode() {
+		return mode;
+	}
+
+	public static TimeMode getTimeMode(final String mode) {
+		for (TimeMode m : TimeMode.values()) {
+			if (m.getMode().equals(mode)) {
+				return m;
+			}
+		}
+		throw new IllegalArgumentException("Unknown mode : " + mode);
+	}
+
+}

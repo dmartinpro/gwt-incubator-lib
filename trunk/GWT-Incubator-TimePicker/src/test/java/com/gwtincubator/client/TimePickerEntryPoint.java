3ea427fc673cb30c36ebb57f1bde2d5938e41d4b
dmartin.pro <dmartin.pro@14f895b6-ede3-11dd-b787-91885b911a96>
2009-02-09 20:50:37 +0000

    code cleaning

--- trunk/GWT-Incubator-TimePicker/src/test/java/com/gwtincubator/client/TimePickerEntryPoint.java
+++ trunk/GWT-Incubator-TimePicker/src/test/java/com/gwtincubator/client/TimePickerEntryPoint.java
@@ -5,14 +5,18 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.gwtincubator.client.timepicker.TimePicker;
+import com.gwtincubator.client.event.PropertyChangeListener;
+import com.gwtincubator.client.event.PropertyChangeSource;
+import com.gwtincubator.client.widget.timepicker.TimePicker;
 
 /**
  * 
- * Sample entry point
+ * TimePicker simple showcase...
  * 
  * @author David MARTIN
  *
@@ -27,23 +31,42 @@ public class TimePickerEntryPoint implements EntryPoint {
 		commandPanel.setSpacing(5);
 		final HorizontalPanel panel = new HorizontalPanel();
 		panel.setSpacing(0);
+		final HorizontalPanel listenersPanel = new HorizontalPanel();
+		listenersPanel.setSpacing(100);
 
-		final TimePicker datePickerField = new TimePicker();
-		datePickerField.setLabel("Date Picker");
-		datePickerField.setTime(11, 45);
-		datePickerField.setWidth("200px");
-		panel.add(datePickerField);
+		final TimePicker timePickerField = new TimePicker();
+		timePickerField.setLabel("Time Picker");
+		timePickerField.setTime(0, 59);
+		timePickerField.setWidth("80px");
+		timePickerField.setTooltip("Pick a time value...");
+		timePickerField.setAccessKey('t');
+		panel.add(timePickerField);
+		final TextBox tb = new TextBox();
+		tb.setReadOnly(true);
+		tb.setText(timePickerField.getValue());
+		listenersPanel.add(new Label("Internal model value : "));
+		listenersPanel.add(tb);
 
-		final Button button = new Button("Get DatePicker's time value");
+		timePickerField.addModelListener(new PropertyChangeListener() {
+			public void propertyChanged(final PropertyChangeSource source,
+					final String propertyName, final Object oldValue, final Object newValue) {
+				if (source != null) {
+					tb.setText(source.toString());
+				}
+			}
+		});
+
+		final Button button = new Button("Get TimePicker's value");
 		button.addClickListener(new ClickListener() {
 			public void onClick(Widget sender) {
-				Window.alert("datePicker field's value is : " + datePickerField.getValue());
+				Window.alert("TimePicker's value is : " + timePickerField.getValue());
 			}
 		});
 		commandPanel.add(button);
 
 		mainPanel.add(commandPanel);
 		mainPanel.add(panel);
+		mainPanel.add(listenersPanel);
 		RootPanel.get().add(mainPanel);
 	}
 
